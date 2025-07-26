97c519ef8ccde239f9a856fcdc03af3e110e8401
Aayush singh <152506577+aayushsing@users.noreply.github.com>
2025-07-26 12:35:21 +0530

    Update script.js
    added some code in script.js

--- script.js
+++ script.js
@@ -7,3 +7,59 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
   alert("Thanks! Your message has been sent.");
   this.reset();
 });
+<script>
+  const slider = document.getElementById("slider");
+  const dotsContainer = document.getElementById("dots");
+  const totalSlides = slider.children.length;
+  let currentSlide = 0;
+  let slideInterval;
+
+  // Create dots
+  for (let i = 0; i < totalSlides; i++) {
+    const dot = document.createElement("span");
+    dot.addEventListener("click", () => goToSlide(i));
+    dotsContainer.appendChild(dot);
+  }
+
+  const dots = dotsContainer.children;
+  updateDots();
+
+  function goToSlide(index) {
+    currentSlide = index;
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    updateDots();
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    goToSlide(currentSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    goToSlide(currentSlide);
+  }
+
+  function updateDots() {
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].classList.remove("active");
+    }
+    dots[currentSlide].classList.add("active");
+  }
+
+  // Auto Slide
+  function startAutoSlide() {
+    slideInterval = setInterval(nextSlide, 4000);
+  }
+
+  function stopAutoSlide() {
+    clearInterval(slideInterval);
+  }
+
+  // Pause on hover
+  const sliderContainer = document.getElementById("slider-container");
+  sliderContainer.addEventListener("mouseover", stopAutoSlide);
+  sliderContainer.addEventListener("mouseout", startAutoSlide);
+
+  startAutoSlide();
+</script>
